49f2a2ab60e102d565d4383bad9f01146d4fb451
Rajesh Bishnoi <rajeshbishnoi@Rajeshs-MacBook-Air.local>
2023-03-15 12:07:09 +0530

    Intial Commit and environment setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/submit", (req, res) => {
+	const { code, language } = req.body;
+	// Handle code submission here
+});
+
+app.listen(3000, () => {
+	console.log("Server started on port 3000");
+});
